d7d2f1d2276afda6c28c7f7951ad8e269bfe75e0
innocent okekem <iokekem@gmail.com>
2025-11-03 05:59:56 -0800

    Initial commit for Smart Attendance System

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,17 @@
 <?php
-$host = '127.0.0.1:3309';
-$db   = 'attendances';
-$user = 'root';
-$pass = '';
+$host = 'blqdfarata3zapjyv6yn-mysql.services.clever-cloud.com';
+$user = 'udoawlwkfiuin9o3';
+$pass = '4DpuR2VCtxbLgj9hYfzw';
+$db   = 'blqdfarata3zapjyv6yn';
+$port = 3306;
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+// echo "Database connected successfully!";
 ?>
+
 
  
